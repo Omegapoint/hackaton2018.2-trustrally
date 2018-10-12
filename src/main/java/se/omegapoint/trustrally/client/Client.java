e986ef10890e92821d7ff7179b2357c35ec2bced
Michel Chamoun <michel.chamoun@omegapoint.se>
2018-10-12 18:49:34 +0200

    Skeleton client

--- src/main/java/se/omegapoint/trustrally/client/Client.java
+++ src/main/java/se/omegapoint/trustrally/client/Client.java
@@ -6,7 +6,6 @@ import se.omegapoint.trustrally.common.io.MessageType;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 import static org.apache.commons.lang3.Validate.notNull;
@@ -14,29 +13,41 @@ import static org.apache.commons.lang3.Validate.notNull;
 public class Client implements Runnable {
 
     private final PlayerType type;
-    private final InetAddress address;
-    private final DatagramSocket socket;
+    private DatagramSocket socket;
 
     private byte[] buf;
 
-    public Client(PlayerType type) throws SocketException {
+    public Client(PlayerType type) {
         this.type = notNull(type);
-        socket = new DatagramSocket();
-        address = InetAddress.getLoopbackAddress();
 
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
-    public String sendMessage(MessageType messageType) throws IOException {
+    public String sendMessage(MessageType messageType) {
         String message = messageType.toString();
         buf = message.getBytes();
         DatagramPacket packetToGameServer = new DatagramPacket(buf, buf.length, 4555); //TODO: Send to address
-        socket.send(packetToGameServer);
+
+        try {
+            socket.send(packetToGameServer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         packetToGameServer = new DatagramPacket(buf, buf.length);
-        socket.receive(packetToGameServer);
+
+        try {
+            socket.receive(packetToGameServer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         String received = new String(packetToGameServer.getData(), 0, packetToGameServer.getLength());
         return received;
-
 
     }
 
