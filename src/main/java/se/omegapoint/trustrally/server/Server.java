bfe8c5f394d68c411dc5f7d2c0b4d56d72cfbe58
Michel Chamoun <michel.chamoun@omegapoint.se>
2018-10-12 16:24:03 +0200

    Server skeleton

--- src/main/java/se/omegapoint/trustrally/server/Server.java
+++ src/main/java/se/omegapoint/trustrally/server/Server.java
@@ -1,9 +1,51 @@
 package se.omegapoint.trustrally.server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Server implements Runnable {
+
+    DatagramSocket connectionSocket;
+    private boolean running;
+    private byte[] buf = new byte[256];
+
+    public Server() {
+        try {
+            connectionSocket = new DatagramSocket(4555); //TODO: Add server address...
+        } catch (SocketException e) {
+            System.out.println("Connection failed...");
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void run() {
-        System.out.println("Running server...");
+        running = true;
+
+        while (running) {
+            try {
+                System.out.println("Running server...");
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                connectionSocket.receive(packet);
+                InetAddress clientAddress = packet.getAddress();
+                int clientPort = packet.getPort();
+                packet = new DatagramPacket(buf, buf.length, clientAddress, clientPort);
+
+                String clientData = new String(packet.getData(), 0, packet.getLength());
+
+                if (clientData.equals("end")) { //TODO: Implement MessageType
+                    running = false;
+                    continue;
+                }
+
+                connectionSocket.send(packet);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
